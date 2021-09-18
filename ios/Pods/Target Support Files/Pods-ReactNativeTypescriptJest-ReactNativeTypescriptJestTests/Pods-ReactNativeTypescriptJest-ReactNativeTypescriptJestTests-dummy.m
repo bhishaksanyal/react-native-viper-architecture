a04c85e5a7b89be64e44a2599f68ca06bba8c372
bhishaksanyal <sanyalbishak93@gmail.com>
2021-09-18 20:38:17 +0530

    Cleanup

--- ios/Pods/Target Support Files/Pods-ReactNativeTypescriptJest-ReactNativeTypescriptJestTests/Pods-ReactNativeTypescriptJest-ReactNativeTypescriptJestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeTypescriptJest_ReactNativeTypescriptJestTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeTypescriptJest_ReactNativeTypescriptJestTests
-@end
